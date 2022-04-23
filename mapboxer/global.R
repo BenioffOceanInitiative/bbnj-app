637ee630bd0166747432157e4b1c5f53cdbc673b
Ben Best <ben@ecoquants.com>
2022-04-22 22:46:24 -0700

    shiny global map with mapbox tileset of hexagons

---
+++ mapboxer/global.R
@@ -0,0 +1,5 @@
+librarian::shelf(
+  bslib, mapboxer, shiny)
+
+mapbox_token_txt <- '~/My Drive/private/mapbox_token_bdbest.txt'
+Sys.setenv("MAPBOX_API_TOKEN" = readLines(mapbox_token_txt))
---
+++ mapboxer/ui.R
@@ -0,0 +1,10 @@
+shinyUI(navbarPage(
+  windowTitle = "BBNJ app",
+  theme = bs_theme(
+    version = 5,
+    "navbar-padding-y" = "0", 
+    "navbar-padding-x" = "0",
+    "container-padding-x" = "0"),
+  #h1("mapboxer"),
+  mapboxerOutput("map", width = "100vw", height = "100vh") ))
+
