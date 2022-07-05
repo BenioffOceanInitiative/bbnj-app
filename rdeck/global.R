e329401a90940f37e452cac1034e7b93dc37da1d
Ben Best <ben@ecoquants.com>
2022-07-05 01:54:18 +0000

    + bslib

--- rdeck/global.R
+++ rdeck/global.R
@@ -4,8 +4,9 @@ if (!require("librarian")){
   library(librarian)
 }
 librarian::shelf(
-  dplyr, qfes/rdeck, sf, viridis)
+  bslib, dplyr, qfes/rdeck, sf, viridis)
 
-mb_token <- readLines("~/My Drive/private/mapbox_token_bdbest.txt")
+# mb_token <- readLines("~/My Drive/private/mapbox_token_bdbest.txt")
+mb_token <- readLines("/share/data/mapbox_token_bdbest.txt")
 options(rdeck.mapbox_access_token = mb_token)
 # rdeck::mapbox_access_token()
